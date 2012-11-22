e33e3b799cf79db05d1a86c0a12adb0c39d77558
Mark Reid <mreid@mozilla.com>
2012-11-22 09:14:08 -0400

    Add a file to control Fx Versions

--- update.sh
+++ update.sh
@@ -10,8 +10,16 @@ KETTLE_DIR=$BASE_DIR/kettle
 
 KETTLE_JOB=$BASE_DIR/run_and_transfer_hif_export.kjb
 
+VERS=$BASE_DIR/fx_versions.txt
+
+VC=$(head -1 $VERS)
+VP1=$(head -2 $VERS | tail -1)
+VP2=$(head -3 $VERS | tail -1)
+VP3=$(head -4 $VERS | tail -1)
+
 cd "$KETTLE_DIR"
 
-./kitchen.sh -file "$KETTLE_JOB" -param:OUTPUT_DIR="$OUTPUT_DIR" -param:REMOTE_OUTPUT_DIR="$REMOTE_OUTPUT_DIR" > "$LOG_FILE"
+./kitchen.sh -file "$KETTLE_JOB" -param:OUTPUT_DIR="$OUTPUT_DIR" -param:REMOTE_OUTPUT_DIR="$REMOTE_OUTPUT_DIR" \
+   -param:V.C=$VC -param:V.P1=$VP1 -param:V.P2=$VP2 -param:V.P3=$VP3 > "$LOG_FILE"
 
 grep -i exception "$LOG_FILE"
