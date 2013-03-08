951f9d9151c067e538d1bb9a7ba3083a363b00d2
Mark Reid <mreid@mozilla.com>
2013-03-08 13:43:53 -0400

    Publish the latest ADI data to a separate location

--- update.sh
+++ update.sh
@@ -3,6 +3,7 @@
 BASE_DIR=/opt/pentaho/kettle/etl/headlines_etl
 OUTPUT_DIR=$BASE_DIR/output/
 REMOTE_OUTPUT_DIR=/var/www/protected/headline/data
+PUB_OUTPUT_DIR=/var/www/html/data
 LOG_FILE=$BASE_DIR/hif_export.log
 
 # This should be a symlink to the desired kettle version
@@ -12,6 +13,9 @@ KETTLE_JOB=$BASE_DIR/run_and_transfer_hif_export.kjb
 
 cd "$KETTLE_DIR"
 
-./kitchen.sh -file "$KETTLE_JOB" -param:OUTPUT_DIR="$OUTPUT_DIR" -param:REMOTE_OUTPUT_DIR="$REMOTE_OUTPUT_DIR" > "$LOG_FILE"
+./kitchen.sh -file "$KETTLE_JOB" \
+   -param:OUTPUT_DIR="$OUTPUT_DIR" \
+   -param:REMOTE_OUTPUT_DIR="$REMOTE_OUTPUT_DIR" \
+   -param:PUB_OUTPUT_DIR="$PUB_OUTPUT_DIR" > "$LOG_FILE"
 
 grep -i exception "$LOG_FILE"
